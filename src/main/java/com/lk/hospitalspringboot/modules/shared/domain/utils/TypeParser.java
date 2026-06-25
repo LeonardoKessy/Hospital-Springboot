7edb574d008979af74673ab6a6029477ce4825ca
Leonardo Kessy <leokessy2005@gmail.com>
2026-06-25 14:59:39 -0300

    Refactor(shared): Allow UUID parsing through TypeParser

--- src/main/java/com/lk/hospitalspringboot/modules/shared/domain/utils/TypeParser.java
+++ src/main/java/com/lk/hospitalspringboot/modules/shared/domain/utils/TypeParser.java
@@ -1,25 +1,36 @@
 package com.lk.hospitalspringboot.modules.shared.domain.utils;
 
+import com.lk.hospitalspringboot.modules.shared.domain.exceptions.InputValidationException;
+
 import java.util.Collection;
 import java.util.Collections;
 import java.util.Set;
+import java.util.UUID;
 import java.util.stream.Collectors;
 
 public final class TypeParser {
 
     private TypeParser() {}
 
-    public static <E extends Enum<E>> E parse(Class<E> enumClass, String value) {
+    public static <E extends Enum<E>> E parseEnum(Class<E> enumClass, String value) {
         if (value == null || value.isBlank()) return null;
         return Enum.valueOf(enumClass, value.toUpperCase().trim());
     }
 
-    public static <E extends Enum<E>> Set<E> parse(Class<E> enumClass, Collection<String> values) {
+    public static <E extends Enum<E>> Set<E> parseEnum(Class<E> enumClass, Collection<String> values) {
         if (values == null || values.isEmpty()) return Collections.emptySet();
         return values.stream()
                 .map(value ->Enum.valueOf(enumClass, value))
                 .collect(Collectors.toUnmodifiableSet());
     }
 
+    public static UUID parseUuid(String uuid) {
+        if (uuid == null || uuid.isBlank()) return null;
+        try  {
+            return UUID.fromString(uuid);
+        }  catch (IllegalArgumentException e) {
+            throw new InputValidationException("id", "Given ID is not a valid UUID");
+        }
+    }
 
 }
